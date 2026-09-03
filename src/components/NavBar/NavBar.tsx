b8178e53232df92e1e8201a95f4c780c2d0145fd
Catalina <catalinayazmincorrea@gmail.com>
2026-09-03 13:00:10 -0300

    starting navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,20 +1,23 @@
-import { useNavigate } from "react-router"
-import { useAuth } from "../../context/AuthContext"
+import { Link } from "react-router"
+import LogoutButton from "../LogoutButton/LogoutButton"
 
 
 const NavBar = () => {
-  const { logout } = useAuth()
-  const navigate = useNavigate()
-
-  const handleLogout = () => {
-    logout()
-    navigate("/login", { replace: true })
-  }
   
   return (
-    <div>
-      <div>NavBar</div>
-      <button onClick={handleLogout}>Logout</button>
+    <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
+      <div>
+        <h3>OutfitMaker ✧</h3>
+      </div>
+      <div className="flex items-center">
+        <Link to="/" className="mr-4 hover:underline">Wardrobe</Link>
+        <Link to="/outfits" className="mr-4 hover:underline">Outfits</Link>
+        <Link to="/categories" className="mr-4 hover:underline">Categories</Link>
+        <Link to="/create-outfit" className="mr-4 hover:underline">Create An Outfit</Link>
+        <Link to="/add-garment" className="mr-4 hover:underline">Add Garment</Link>
+
+        <div className="w-30 flex flex-row-reverse"><LogoutButton /></div>
+      </div>
     </div>
   )
 }
